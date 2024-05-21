f272b195fbda962f8b1ead60de86d0a202337657
Logan McAnsh <logan@mcan.sh>
2024-05-21 15:54:36 -0400

    x
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,7 +4,7 @@
     <ul class="list-disc space-y-2 pt-6">
         @foreach ($jobs as $job)
             <li>
-                <a href="/jobs/{{ $job["id"] }}">
+                <a href="/jobs/{{ $job["id"] }}" class="text-blue-500">
                     <strong>
                         {{ $job["title"] }}: Pays ${{ $job["salary"] }} per
                         year.
