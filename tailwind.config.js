8b177f795102b3be7c4c336db4aef3caf329108f
Logan McAnsh <logan@mcan.sh>
2024-05-21 16:51:20 -0400

    chore: tailwind forms plugin
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import forms from "@tailwindcss/forms";
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
@@ -8,5 +10,5 @@ export default {
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [forms],
 };
