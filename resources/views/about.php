b5fdd6a62de947a3625de051cf1822dbc3e78e6e
Logan McAnsh <logan@mcan.sh>
2024-05-21 13:53:07 -0400

    chore: edit welcome route and add about.php route

---
+++ resources/views/about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<title>About</title>
+	</head>
+	<body>
+		<h1>About</h1>
+		<p>This is a simple website that demonstrates how to use PHP and MySQL.</p>
+	</body>
+</html>
