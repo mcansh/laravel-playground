f9528621ec6b999f9094da3a2ff2813163242732
Logan McAnsh <logan@mcan.sh>
2024-05-22 02:14:42 -0400

    chore: add page titles
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,10 +1,15 @@
+@props([
+    "title",
+    "heading",
+])
+
 <!DOCTYPE html>
 <html lang="{{ str_replace("_", "-", app()->getLocale()) }}" class="h-full">
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Laravel</title>
+        <title>{{ $title ?? $heading }} | {{ config("app.name") }}</title>
 
         @vite("resources/css/app.css")
     </head>
