3d9e8ea7dad76c61742bc37da731fee77f93c626
Logan McAnsh <logan@mcan.sh>
2024-05-23 15:22:34 -0400

    chore: better looking, but inconsistently sized cards
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -21,32 +21,65 @@
             </a>
         </div>
 
-        <ul class="list-disc space-y-2 pl-4">
+        <div class="grid grid-cols-4 gap-4">
             @foreach ($jobs as $job)
-                <li>
-                    <strong>{{ $job->position }}</strong>
-                    <p>
-                        Company:
-                        <a
-                            class="text-indigo-600"
-                            href="{{ route("employers.show", $job->employer) }}"
+                <div
+                    class="rounded-lg bg-gray-50 shadow-sm ring-1 ring-gray-900/5"
+                >
+                    <dl class="flex flex-wrap">
+                        <div class="flex-auto pl-6 pt-6">
+                            <dt
+                                class="text-sm font-semibold leading-6 text-gray-900"
+                            >
+                                {{ $job->position }}
+                            </dt>
+                            <dd
+                                class="mt-1 text-base font-semibold leading-6 text-gray-900"
+                            >
+                                {{ $job->salary }}
+                            </dd>
+                        </div>
+                        <div class="flex-none self-end px-6 pt-4">
+                            <dt class="sr-only">Status</dt>
+                            <dd
+                                class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+                            >
+                                Hiring
+                            </dd>
+                        </div>
+                        <div
+                            class="mt-6 flex w-full flex-none gap-x-4 border-t border-gray-900/5 px-6 pt-6"
                         >
-                            {{ $job->employer->name }}
+                            <dt class="flex-none">
+                                <span class="sr-only">Employer</span>
+                            </dt>
+                            <dd
+                                class="text-sm font-medium leading-6 text-gray-900"
+                            >
+                                {{ $job->employer->name }}
+                            </dd>
+                        </div>
+                        <div class="mt-4 flex w-full flex-none gap-x-4 px-6">
+                            <dt class="flex-none">
+                                <span class="sr-only">Location</span>
+                            </dt>
+                            <dd class="text-sm leading-6 text-gray-500">
+                                {{ $job->location }}
+                            </dd>
+                        </div>
+                    </dl>
+                    <div class="mt-6 border-t border-gray-900/5 px-6 py-6">
+                        <a
+                            href="{{ route("jobs.show", $job) }}"
+                            class="text-sm font-semibold leading-6 text-gray-900"
+                        >
+                            View Listing
+                            <span aria-hidden="true">&rarr;</span>
                         </a>
-                    </p>
-
-                    <p>Location: {{ $job->location }}</p>
-                    <p>Salary: {{ $job->salary }}</p>
-                    <a
-                        class="text-blue-500"
-                        href="{{ route("jobs.show", $job) }}"
-                    >
-                        View
-                        <span aria-hidden="true">&rarr;</span>
-                    </a>
-                </li>
+                    </div>
+                </div>
             @endforeach
-        </ul>
+        </div>
 
         {{ $jobs->links() }}
     </div>
