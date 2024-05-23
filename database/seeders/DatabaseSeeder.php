2654aad8cb70dafde0b647a572e2e9a891077ec8
Logan McAnsh <logan@mcan.sh>
2024-05-23 15:47:07 -0400

    chore: add Jobs and Employers to db seeder
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employer;
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,7 +20,16 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             "first_name" => "John",
             "last_name" => "Doe",
-            "email" => "test@example.com",
+            "email" => fake()->unique()->safeEmail(),
         ]);
+
+        // create 100 jobs at 1-10 jobs per employer
+        Employer::factory(10)
+            ->create()
+            ->each(function ($employer) {
+                Job::factory(random_int(1, 10))->create([
+                    "employer_id" => $employer->id,
+                ]);
+            });
     }
 }
