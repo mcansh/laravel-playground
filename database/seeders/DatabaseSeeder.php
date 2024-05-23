51c9db864353fb00c2c7aa6e382d19ec2dde90b9
Logan McAnsh <logan@mcan.sh>
2024-05-23 15:37:06 -0400

    chore: update db seed to match model
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            "name" => "Test User",
+            "first_name" => "John",
+            "last_name" => "Doe",
             "email" => "test@example.com",
         ]);
     }
