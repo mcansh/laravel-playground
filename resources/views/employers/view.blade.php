a7e312aa3b28b532d060107c6606cad5e029ecb6
Logan McAnsh <logan@mcan.sh>
2024-05-23 15:47:27 -0400

    chore: use grid for employers.show page
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- resources/views/employers/view.blade.php
+++ resources/views/employers/view.blade.php
@@ -22,14 +22,64 @@
             </p>
         </div>
 
-        <ul>
+        <div class="grid grid-cols-4 gap-4">
             @foreach ($employer->jobs as $job)
-                <li>
-                    <a href="{{ route("jobs.show", $job) }}">
-                        {{ $job->position }}
-                    </a>
-                </li>
+                <div
+                    class="rounded-lg bg-gray-50 shadow-sm ring-1 ring-gray-900/5"
+                >
+                    <dl class="flex flex-wrap">
+                        <div class="flex-auto pl-6 pt-6">
+                            <dt
+                                class="text-sm font-semibold leading-6 text-gray-900"
+                            >
+                                {{ $job->position }}
+                            </dt>
+                            <dd
+                                class="mt-1 text-base font-semibold leading-6 text-gray-900"
+                            >
+                                {{ $job->salary }}
+                            </dd>
+                        </div>
+                        <div class="flex-none self-end px-6 pt-4">
+                            <dt class="sr-only">Status</dt>
+                            <dd
+                                class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20"
+                            >
+                                Hiring
+                            </dd>
+                        </div>
+                        <div
+                            class="mt-6 flex w-full flex-none gap-x-4 border-t border-gray-900/5 px-6 pt-6"
+                        >
+                            <dt class="flex-none">
+                                <span class="sr-only">Employer</span>
+                            </dt>
+                            <dd
+                                class="text-sm font-medium leading-6 text-gray-900"
+                            >
+                                {{ $employer->name }}
+                            </dd>
+                        </div>
+                        <div class="mt-4 flex w-full flex-none gap-x-4 px-6">
+                            <dt class="flex-none">
+                                <span class="sr-only">Location</span>
+                            </dt>
+                            <dd class="text-sm leading-6 text-gray-500">
+                                {{ $job->location }}
+                            </dd>
+                        </div>
+                    </dl>
+                    <div class="mt-6 border-t border-gray-900/5 px-6 py-6">
+                        <a
+                            href="{{ route("jobs.show", $job) }}"
+                            class="text-sm font-semibold leading-6 text-gray-900"
+                        >
+                            View Listing
+                            <span aria-hidden="true">&rarr;</span>
+                        </a>
+                    </div>
+                </div>
             @endforeach
-        </ul>
+        </div>
     </div>
 </x-layout>
