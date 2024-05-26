e603e6d8cd110bfc6e71451fa68186fbe0d3e0f0
Logan McAnsh <logan@mcan.sh>
2024-05-26 16:07:18 -0400

    chore: explicit route definition with middleware
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,63 @@ Route::view("/", "home")->name("home");
 
 Route::view("/contact", "contact");
 
-Route::resource("jobs", JobController::class);
+Route::get("/jobs", [JobController::class, "index"])->name("jobs.index");
+Route::get("/jobs/create", [JobController::class, "create"])->name(
+    "jobs.create",
+);
+Route::post("/jobs", [JobController::class, "store"])
+    ->name("jobs.store")
+    ->middleware("auth");
+Route::get("/jobs/{job}", [JobController::class, "show"])->name("jobs.show");
+Route::get("/jobs/{job}/edit", [JobController::class, "edit"])
+    ->name("jobs.edit")
+    ->middleware(["auth", "can:edit-job,job"]);
+Route::put("/jobs/{job}", [JobController::class, "update"])
+    ->name("jobs.update")
+    ->middleware(["auth", "can:edit-job,job"]);
+Route::delete("/jobs/{job}", [JobController::class, "destroy"])
+    ->name("jobs.destroy")
+    ->middleware(["auth", "can:edit-job,job"]);
+Route::get("/employers", [EmployerController::class, "index"])->name(
+    "employers.index",
+);
+Route::get("/employers/create", [EmployerController::class, "create"])->name(
+    "employers.create",
+);
+Route::post("/employers", [EmployerController::class, "store"])->name(
+    "employers.store",
+);
+Route::get("/employers/{employer}", [EmployerController::class, "show"])->name(
+    "employers.show",
+);
+Route::get("/employers/{employer}/edit", [
+    EmployerController::class,
+    "edit",
+])->name("employers.edit");
+Route::put("/employers/{employer}", [
+    EmployerController::class,
+    "update",
+])->name("employers.update");
+Route::delete("/employers/{employer}", [
+    EmployerController::class,
+    "destroy",
+])->name("employers.destroy");
 
-Route::resource("employers", EmployerController::class);
-
-Route::resource("tags", TagController::class, [
-    "only" => ["index", "show", "edit", "update", "destroy"],
-]);
+Route::get("/tags", [TagController::class, "index"])->name("tags.index");
+Route::get("/tags/create", [TagController::class, "create"])->name(
+    "tags.create",
+);
+Route::post("/tags", [TagController::class, "store"])->name("tags.store");
+Route::get("/tags/{tag}", [TagController::class, "show"])->name("tags.show");
+Route::get("/tags/{tag}/edit", [TagController::class, "edit"])->name(
+    "tags.edit",
+);
+Route::put("/tags/{tag}", [TagController::class, "update"])->name(
+    "tags.update",
+);
+Route::delete("/tags/{tag}", [TagController::class, "destroy"])->name(
+    "tags.destroy",
+);
 
 // Auth
 Route::get("register", [RegisteredUserController::class, "create"])->name(
